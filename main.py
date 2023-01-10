61141e253104c36ea3072430b230bb16e92ac680
David Li <davidli012345@gmail.com>
2023-01-09 22:17:56 -0800

    fix: update main

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ drive = deta.Drive("stonk_events")
 
 app.add_middleware(
     CORSMiddleware,
-    # allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
