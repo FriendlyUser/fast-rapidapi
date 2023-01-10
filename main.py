6a06a5ec0b209ef6077b477f3c773e4864880c2b
David Li <davidli012345@gmail.com>
2023-01-09 21:13:05 -0800

    fix: update requirements.txt

--- main.py
+++ main.py
@@ -1,38 +1,60 @@
 from fastapi import FastAPI
 import pandas as pd
+import os
+from deta import Deta
+from fastapi.responses import StreamingResponse, FileResponse
+from io import BytesIO
 
 from fastapi.middleware.cors import CORSMiddleware
 
-origins = [
-    "https://stonk-list-api.p.rapidapi.com",
-    "http://localhost:3000"
-]
-
 app = FastAPI()
 
+# load deta using 
+project_key = os.getenv("DETA_DRIVE_KEY")
+deta = Deta(project_key)
+drive = deta.Drive("stonk_events")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    # allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-us_tickers = pd.read_csv(
-                "https://raw.githubusercontent.com/dli-invest/eod_tickers/main/data/us_stock_data.csv", index_col=False
-            )
+@app.get("/files")
+async def get_files(exchange: str = "US"):
+    """
+    """
+    # get files in deta
+    result = drive.list()
+    all_files = result.get("names")
+    paging = result.get("paging")
+    last = paging.get("last") if paging else None
 
-cad_tickers = pd.read_csv(
-            "https://raw.githubusercontent.com/FriendlyUser/cad_tickers_list/main/static/latest/stocks.csv", index_col=False
-        )
-@app.get("/tickers")
-async def get_spreadsheet(exchange: str = "US"):
-    """
-    """
-    if exchange == "US":
-    # return spreadsheets from github here
-        return us_tickers.to_csv()
-    else: 
-        return cad_tickers.to_csv()
+    while (last):
+        # provide last from previous call
+        result = drive.list(last=last)
+
+        all_files += result.get("names")
+        # update last
+        paging = result.get("paging")
+        last = paging.get("last") if paging else None
+    return all_files
+
+
+# get file by name from deta /file/{id}
+@app.get("/file/{id}")
+async def get_file(id: str):
+    output_file = drive.get(id)
+    # return file
+    # content = output_file.read()
+    
+    content = output_file.read()
+    output_file.close()
+    headers = {
+        'Content-Disposition': f'inline; filename="{id}"'
+    }
+    bytesio_object = BytesIO(content)
+    return StreamingResponse(bytesio_object, headers=headers)
 
